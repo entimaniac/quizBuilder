866d54f36e923b83a7bf4c00ca5c45aaa7df9b3c
Jesse Strode <jesse_strode@condenast.com>
2020-04-30 22:11:17 -0400

    Updates

---
+++ precache-manifest.303a9a939388a57b3089dfd53e1df144.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a71dda323c139704db62a6d553d8ea4",
+    "url": "/quizBuilder/index.html"
+  },
+  {
+    "revision": "b22e8cad918907d2a5f3",
+    "url": "/quizBuilder/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c7a180457779b0edd333",
+    "url": "/quizBuilder/static/js/2.84a2df85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizBuilder/static/js/2.84a2df85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b22e8cad918907d2a5f3",
+    "url": "/quizBuilder/static/js/main.a57c090d.chunk.js"
+  },
+  {
+    "revision": "1fd878118a08b20d80c5",
+    "url": "/quizBuilder/static/js/runtime-main.6aa125f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quizBuilder/static/media/logo.5d5d9eef.svg"
+  }
+]);
